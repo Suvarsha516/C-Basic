ff7c65b92e6fb32dfef0df038eab753b972c513d
Suvarsha516 <93140526+Suvarsha516@users.noreply.github.com>
2021-11-08 11:50:30 +0530

    Add files via upload

---
+++ relational comparisional operators.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int a, b;
+	a=50;
+	b=20;
+	printf("a=%d\nb=%d\n",a,b);
+	printf("%d > %d -->%d\n",a,b, a>b);
+	printf("%d < %d -->%d\n",a,b, a<b);
+	printf("%d >= %d -->%d\n",a,b, a>=b);
+	printf("%d <= %d -->%d\n",a,b, a<=b);
+	printf("%d == %d -->%d\n",a,b, a==b);
+	printf("%d != %d -->%d\n",a,b, a!=b);
+	
+}
